f4e632606d6655a3581be9a8c0efe79e4c24bf2c
Hannes Datta <h.datta@tilburguniversity.edu>
2023-03-17 14:49:53 +0100

    update 2022

--- static/data/update_database.R
+++ static/data/update_database.R
@@ -3,7 +3,7 @@ library(googledrive)
 library(data.table)
 library(jsonify)
 
-drive_download(as_id('1TU1q_jXhUXsVcyxCr9fYGD9mK6j3he5f'), path =  'raw_coding.xls', overwrite=T)
+drive_download(as_id('1TU1q_jXhUXsVcyxCr9fYGD9mK6j3he5f'), path =  'raw_coding.xlsx', overwrite=T)
 
 raw_coding <- data.table(readxl::read_xlsx('raw_coding.xlsx'))
 raw_coding[, DOI:=gsub('\n', '', DOI)]
@@ -19,7 +19,6 @@ public_coding[grepl('JCR', journal), journal_long := 'Journal of Consumer Resear
 public_coding[grepl('JM', journal), journal_long := 'Journal of Marketing']
 public_coding[grepl('MktSci', journal), journal_long := 'Marketing Science']
 public_coding[grepl('JMR', journal), journal_long := 'Journal of Marketing Research']
-
 public_coding[is.na(scraped), scraped:=0]
 public_coding[is.na(api), api:=0]
 
@@ -31,8 +30,10 @@ public_coding[scraped==0&api==0, type:= 'Manual extraction']
 public_coding[scraped==1&api==0, type:= 'Web scraping']
 public_coding[scraped==0&api==1, type:= 'API']
 public_coding[scraped==1&api==1, type:= 'Both web scraping and API']
+public_coding[year>=2021, type:='Not classified whether web scraping or API']
 
 public_coding <- public_coding[, c('year','journal','doi','authors','title', 'type')]
+public_coding = unique(public_coding, by = c('doi'))
 
 sink('bib.json')
 to_json(public_coding, digits=0, unbox=T)
